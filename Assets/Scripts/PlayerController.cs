9fd73c01dafc2f380d2ee211aa9cf6e2a1145e06
Frank <fnu.frangky@gmail.com>
2017-06-15 11:46:41 -0400

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    private void Start() //gets callend script is run
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //input from keyboard?
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); // dont want to move up so its 0
+
+
+        rb.AddForce (movement * speed);
+
+
+    }
+}
